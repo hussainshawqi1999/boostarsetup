84ed2f1a4753378bac417dad0233bd5b158c7a87
hussainshawqi1999 <hussainshawqi4@gmail.com>
2025-12-24 09:54:51 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,4 @@
 import './globals.css'
-
-export const metadata = {
-  title: 'Stremio Auto Setup - By Hussain',
-  description: 'Professional Stremio Addon Manager',
-}
-
 export default function RootLayout({ children }) {
-  return (
-    <html lang="ar">
-      <body>{children}</body>
-    </html>
-  )
+  return (<html lang="ar"><body>{children}</body></html>)
 }
